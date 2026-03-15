010891a73df5bd34aad96a0e172906acb679d28d
Rafael Vargas <rafael1199v@gmail.com>
2026-03-15 14:35:10 -0400

    feat: Adding types for order creation

---
+++ src/feature/sales/types/order.ts
@@ -0,0 +1,24 @@
+
+export type CreateOrderRequest = {
+	companyId: number;
+};
+
+export type CreateOrderResponse = {
+	restaurantOrderId: number;
+};
+
+export type RestaurantOrder = {
+	id: number;
+	dailyNumber: number;
+	orderDatetime: string;
+	orderStatusId: number;
+	customerId?: number | null;
+	taxPrice: number;
+	restaurantOrderId: number;
+	waiterId?: number | null;
+};
+
+export type Waiter = {
+	id: number;
+	name: string;
+};
